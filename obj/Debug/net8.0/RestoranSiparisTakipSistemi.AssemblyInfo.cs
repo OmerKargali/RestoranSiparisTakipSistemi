c7443c325e0daa8d8a15dfbc831010268ce70c3d
ahmetcaliskan63 <ahmetabclife@gmail.com>
2024-11-28 14:10:20 +0300

    sepet kısmı tamamlandı

--- obj/Debug/net8.0/RestoranSiparisTakipSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestoranSiparisTakipSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c89cb9e6f201996205643dcdb4cb1e2df21ff0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa77a3f6ded3b4ee9a4770250bd14626c87ace08")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
