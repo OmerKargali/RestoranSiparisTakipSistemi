74de5e8d9e63e08ba97ee4e662f7413f55cc8b35
OmerKargali <omer_kargalii@icloud.com>
2024-11-30 16:07:00 +0300

    Çakışma sorunları çözüldü

--- obj/Debug/net8.0/RestoranSiparisTakipSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestoranSiparisTakipSistemi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636653edb418783e70b0aa896acc7311dfdc0e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc4df01a7be8eb73a6fbd91765771d03002d711")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestoranSiparisTakipSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
